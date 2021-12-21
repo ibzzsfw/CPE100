aa822dc938d8e4a77d21fa21b351a67ab30affa8
ibzzsfw <suppakorn.rakna@mail.kmutt.ac.th>
2021-12-21 17:15:14 +0700

    add assignment3

---
+++ assignment3/Assignment3.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+  int x,y,count;
+  char ch;
+  char name[30];
+//Interactive
+  printf("Welcome to Number guessing game!!乁[ ◕ ᴥ ◕ ]ㄏ\n");
+  printf("What's your name!??? : ");
+  scanf("%s",name);
+  printf("\n(ﾉ´∀｀)ﾉ ... Hello ... %s ヾ(´▽｀;)ゝ\n",name);
+//Loop program
+  do
+  {
+    srand((unsigned int)time(NULL));
+    x=rand()%100+1;
+    count=0;
+//Test program
+    printf("x= %d\n",x);
+//Guess game
+    do
+    {
+      count++;
+      printf("\nGuess round : %d\n",count);
+      scanf("%d",&y);
+      if (x==y)
+      {
+        printf("Yes!! Bravo! Why are you so GOD ヽ|･з･|ゞ\n");
+      }
+      else
+      {
+        if (y<x)
+        {
+          printf("Your guess is too low");
+        }
+        else
+        {
+          printf("Your guess is too high");
+        }
+        if (count==7)
+        {
+          printf(" and you lose, but it's OK babe...\nAnswer is %d hahaha eiei\n",x);
+        }
+      }
+    }
+    while (x!=y&&count<7);
+    do
+    {
+      printf("Enter y to play again or n to exit.");
+      scanf(" %c",&ch);
+    }
+    while(ch!='y'&&ch!='Y'&&ch!='n');
+  }
+  while (ch == 'y'||ch=='Y');
+    
+  return 0;
+}
---
+++ assignment3/Assignment3.h
@@ -0,0 +1,14 @@
+//
+//  Assignment3.h
+//  Assignment3
+//
+//  Created by suppakorn rakna on 27/8/2563 BE.
+//  Copyright © 2563 suppakorn rakna. All rights reserved.
+//
+
+#ifndef Assignment3_h
+#define Assignment3_h
+
+#include <stdio.h>
+
+#endif /* Assignment3_h */
