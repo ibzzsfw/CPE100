3702a3f972a7aa3a0e42e4cb35731bebc5b2f2c0
ibzzsfw <suppakorn.rakna@mail.kmutt.ac.th>
2021-12-21 17:13:27 +0700

    add assignment2

---
+++ assignment2/Assignment2.h
@@ -0,0 +1,14 @@
+//
+//  Assignment2.h
+//  Assignment2
+//
+//  Created by suppakorn rakna on 27/8/2563 BE.
+//  Copyright © 2563 suppakorn rakna. All rights reserved.
+//
+
+#ifndef Assignment2_h
+#define Assignment2_h
+
+#include <stdio.h>
+
+#endif /* Assignment2_h */
